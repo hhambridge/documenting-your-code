73b6dc9ed82b08039b098bc870ac3cb09925e547
Hali Hambridge <hhambridge@g.harvard.edu>
2022-04-28 14:28:42 -0600

    Updating with comments and type annotations.

--- randomly/facts.py
+++ randomly/facts.py
@@ -1,18 +1,22 @@
 import requests
 from requests.exceptions import RequestException
+from typing import Union
 
 
-def generate_random_fact(output_format, language):
+def generate_random_fact(output_format: str, language: str) -> Union[str, dict]:
+    # Verify inputs are valid
     if language not in {"en", "de"}:
         raise ValueError(f"{language} is not supported.")
 
     if output_format not in {"html", "json", "txt", "md"}:
         raise ValueError(f"{output_format} is not supported.")
 
+    # Query to get fact
     response = requests.get(
         f"https://uselessfacts.jsph.pl/random.{output_format}?language={language}"
     )
 
+    # Status code 200 indicates successful request
     if response.status_code == 200:
         if output_format == "json":
             fact = response.json()
